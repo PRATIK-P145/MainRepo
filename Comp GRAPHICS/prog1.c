846add43a5f648a7829d5e7441697b5c99fd3ab6
Pratik <pratikpatil0307@gmail.com>
2025-01-20 07:28:08 +0530

    Update DDALine.c

--- Comp GRAPHICS/prog1.c
+++ Comp GRAPHICS/prog1.c
@@ -1,51 +1,51 @@
-#include<stdio.h>
-#include<GL/glut.h>
+#include <stdio.h>
+#include <GL/glut.h>
 
 int xa, ya, xb, yb;
 
-void DDALine(int a,int b, int c, int d){
-  glBegin(GL_LINES);
-  glVertex2d(a,b);
-  glVertex2d(c,d);
+void DDALine(int a, int b, int c, int d) {
+    float dx = c - a;
+    float dy = d - b;
+    int steps = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
+
+    float xIncrement = dx / steps;
+    float yIncrement = dy / steps;
+
+    float x = a, y = b;
+
+    glBegin(GL_POINTS);
+    for (int i = 0; i <= steps; i++) {
+        glVertex2d(x, y);
+        x += xIncrement;
+        y += yIncrement;
+    }
+    glEnd();
 }
 
-void Draw()
-{
-  glClear(GL_COLOR_BUFFER_BIT);
-  DDALine();
-
-  glEnd();
-  glFlush();
-
+void Draw() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    DDALine(xa, ya, xb, yb);
+    glFlush();
 }
 
+int main(int argc, char **argv) {
+    printf("Enter first point (xa, ya): ");
+    scanf("%d%d", &xa, &ya);
 
-int main(int argc,char** argv)
-{
+    printf("Enter second point (xb, yb): ");
+    scanf("%d%d", &xb, &yb);
 
-printf("First :");
-scanf("%d%d",xa,ya);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("DDA Line Drawing");
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3f(0.0, 0.0, 0.0);
+    gluOrtho2D(0, 640, 0, 480);
 
-printf("Second :");
-scanf("%d%d",xb,yb);
+    glutDisplayFunc(Draw);
+    glutMainLoop();
 
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-glutInitWindowPosition(0,0);
-glutInitWindowSize(640,480);
-glutCreateWindow("Window1");
-glClearColor(1.0,1.0,1.0,0);
-glColor3f(0.1,0.1,0.1);
-gluOrtho2D(0,640,0,480);
-
-glutDisplayFunc(Draw);
-glutMainLoop();
-
-
-
-return 0;
-
-
-
-
+    return 0;
 }
