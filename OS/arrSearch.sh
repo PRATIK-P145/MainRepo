ad783e233a2ef14efcd240cffb6cb9974847b37d
Pratik Patil <pratikpatil0307@gmail.com>
2025-10-07 22:45:45 +0530

    Create arrSearch.sh

---
+++ OS/arrSearch.sh
@@ -0,0 +1,40 @@
+#arr=(2 3 14 54 82 23)
+#n=6 
+flag=0
+echo -e Enter No. of elements :
+read n
+
+echo -e Enter $n Values :
+
+for (( i=0 ; i<n ; i++ ))
+do 
+ read temp
+ arr[$i]=$temp
+done
+
+echo -e "Array is : ${arr[@]} \n"
+
+echo -e  Enter VAlue to be searched : 
+read num
+
+for (( i=0 ; i<n ; i++ ))
+do
+
+ if(( $num==${arr[$i]} ))
+ then
+  #echo -e Number Found at index $i
+  flag=1
+  break
+ fi
+ 
+done
+
+
+
+if (( $flag==1 ))
+then
+echo -e Number Found at index $i
+else
+echo -e Number Not Found !
+
+fi
