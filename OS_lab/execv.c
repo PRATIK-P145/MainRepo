8ea5cdbf76c6a95229861f309299caf5f3682a46
Pratik Patil <pratikpatil0307@gmail.com>
2025-11-06 07:46:55 +0530

    Update execv.c

--- OS_lab/execv.c
+++ OS_lab/execv.c
@@ -4,49 +4,56 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s ./b.out num1 num2 num3 ...\n", argv[0]);
+        return 1;
+    }
 
-void main(int argc, char* argv[]){
+    // Sort numeric arguments (indices 2 to argc-1)
+    for (int i = 2; i < argc - 1; i++) {
+        for (int j = 2; j < argc - i + 1; j++) {
+            if (atoi(argv[j]) > atoi(argv[j + 1])) {
+                char* temp = argv[j];
+                argv[j] = argv[j + 1];
+                argv[j + 1] = temp;
+            }
+        }
+    }
 
-	//char* temp[] = {"./b.out", "11","12","2","4", NULL};
-	
-	for(int i=0; i<=4; i++){
-		for(int j=0; j<=4; j++){
-			if( atoi(argv[j])>atoi(argv[j+1]) ){
-				char* t;
-				t = argv[j];
-				argv[j] = argv[j+1];
-				argv[j+1] = t;
-			}
-		}
-	}
-	
-	int status;
-	pid_t pid;
-	pid = fork();
-	
-	if(pid==0){
-		printf("\n");
-		printf("\nParent ID : %d",getppid());
-		printf("\nThis is a Child Process, ID :%d\n", getpid());
-		
-		
-		execv(argv[1], argv);
-		printf("\nExit from Child\n");
-		
-	
-	}else{
-		wait(&status);
-		
-		printf("\n");
-		printf("\nThis is a Parent Process, ID :%d ", getpid());
-		printf("\nChild ID : %d\n",pid);
-		
-		for(int i=2; i<argc; i++){
-			printf("%d ",atoi(argv[i]));
-	        }			
-	}
+    pid_t pid = fork();
+    int status;
 
+    if (pid == 0) {
+        printf("\nParent ID : %d", getppid());
+        printf("\nThis is Child Process, ID : %d\n", getpid());
+
+        // Build argument list for execv
+        char* args_for_b[argc - 1];
+        args_for_b[0] = argv[1];
+        for (int i = 2; i < argc; i++) {
+            args_for_b[i - 1] = argv[i];
+        }
+        args_for_b[argc - 1] = NULL;
+
+        execv(argv[1], args_for_b);
+        perror("execv failed");
+        exit(1);
+    } else {
+        wait(&status);
+        printf("\nThis is Parent Process, ID : %d", getpid());
+        printf("\nChild ID : %d\n", pid);
+
+        printf("Sorted numbers (ascending): ");
+        for (int i = 2; i < argc; i++) {
+            printf("%d ", atoi(argv[i]));
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
+
  /* Inside b.c :
  
 #include <stdio.h>
